b98bfc64ffd82df2dad5c9a24a3aabd142379e84
Yovarni Yearwood <thenerfknight@gmail.com>
2019-09-04 17:51:21 -0400

    Fixed up Team object for import and export

--- models/team.py
+++ models/team.py
@@ -5,34 +5,46 @@ import json
 class Team:
     name = ""
     owner = ""
-    overall = 0
-
     carManufacturer = ""
 
     equipmentRating = 0
+    teamRating = 0
+    raceRating = 0
 
-    wins = 0
-    dnf = 0
-
-    sponsors = []
+    # TODO: Implement these features
+    # wins = 0
+    # dnf = 0
+    # poles = 0
+    # sponsors = []
     drivers = []
 
     momentum = 0
 
     def __init__(self, team):
-        self.name = team['Name']
-        self.owner = team['Owner']
-        self.overall = team['Overall']
-        self.drivers = team['Drivers']
+        if type(team) == dict:
+            self.name = team['name']
+            self.owner = team['owner']
+            self.carManufacturer = team['carManufacturer']
+            self.equipmentRating = team['equipmentRating']
+            self.teamRating = team['teamRating']
+            self.raceRating = team['raceRating']
+        else:
+            self.name = team[0]
+            self.owner = team[1]
+            self.carManufacturer = team[2]
+            self.equipmentRating = int(team[3])
+            self.teamRating = int(team[4])
+            self.raceRating = int(team[5])
 
     # TODO replace str() with string.format()
     def toString(self):
         print("Team Name:", self.name,
               "\nOwner:", self.owner,
-              "\nOverall:", str(self.overall),
+              "\ncarManufacturer:", self.carManufacturer,
+              "\nequipmentRating:", "".join(self.equipmentRating),
+              "\nteamRating:", "".join(self.teamRating),
+              "\nraceRating:", "".join(self.raceRating),
               "\nDrivers:", "".join(self.drivers))
 
-    # TODO: Finish correct implementation of toJSON()
     def toJSON(self):
-        with open('teams.json', 'w') as teamsJSON:
-            json.dump(self, teamsJSON, indent=4)
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
