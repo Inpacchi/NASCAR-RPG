e176d6332e0fcd3ce9ff1ab903179d260acda974
Yovarni Yearwood <thenerfknight@gmail.com>
2019-09-05 00:14:32 -0400

    Updated potential.py to make use of new JSON functionality

--- potential.py
+++ potential.py
@@ -1,28 +1,81 @@
 import json
+import fileutilities as futil
 
 # Global Pseudo-Private Variable Declaration
 __progressionDict = {}
 __regressionDict = {}
 
+__driverPotentialDict = {}
 
-def __loadPotentialDicts():
-    with open('data/json/potential.json', 'r') as potentialJSON:
-        __progressionDict.update(json.load(potentialJSON))
+
+def processStage(driversList, standingsDict):
+    driversDict = futil.readDictFromJSON('currentdrivers')
+    __getPotentialDicts()
+    __getDriverPotentialDict(driversDict)
+    __calculateDriverPotential(driversDict, standingsDict)
+
+
+def __getPotentialDicts():
+    with open('data/json/progression.json', 'r') as progressionJSON:
+        __progressionDict.update(json.load(progressionJSON))
 
     with open('data/json/regression.json', 'r') as regressionJSON:
         __regressionDict.update(json.load(regressionJSON))
 
 
-def getPotentialRates():
-    potentialRates = []
+def __getDriverPotentialDict(driversDict):
+    for driver in driversDict:
+        tempDict = {
+            driver: {
+                "age": driversDict[driver].age,
+                "potential": driversDict[driver].potential
+            }
+        }
 
-    # for driver in driversList:
-    #     potentials.append(driver.potential)
-
-    return potentials
+        __driverPotentialDict.update(tempDict)
 
 
-def definePotential(driver):
-    progression = ''
-    ageRange = ''
-    regression = ''
+# TODO: Update proper progression/regression training
+def __calculateDriverPotential(driversDict, standingsDict):
+    for driver in __driverPotentialDict:
+        string = __driverPotentialDict[driver]['potential'].split(';')
+        progression = string[0]
+        ageRange = string[1]
+        regression = string[2]
+
+        standingPlacement = determineStandingPlacement(standingsDict[driver]['finishingPosition'])
+
+        rate = __progressionDict[progression][standingPlacement]
+
+        overallRating = float(driversDict[driver].overallRating)
+        overallRating += rate
+        driversDict[driver].overallRating = str(overallRating)
+
+    futil.writeDictToJSON('currentdrivers', driversDict)
+
+
+def determineStandingPlacement(finishingPosition):
+    if finishingPosition == 1:
+        return 'first'
+    elif 2 <= finishingPosition <= 3:
+        return 'top3'
+    elif 4 <= finishingPosition <= 5:
+        return 'top5'
+    elif 6 <= finishingPosition <= 10:
+        return 'top10'
+    elif 11 <= finishingPosition <= 15:
+        return 'top15'
+    elif 16 <= finishingPosition <= 20:
+        return 'top20'
+    elif 21 <= finishingPosition <= 25:
+        return 'top25'
+    elif 26 <= finishingPosition <= 30:
+        return 'top30'
+    elif 31 <= finishingPosition <= 35:
+        return 'top35'
+    elif 36 <= finishingPosition <= 40:
+        return 'top40'
+    elif 41 <= finishingPosition:
+        return 'DNF'
+    elif finishingPosition == 'DNQ':
+        return 'DNQ'
