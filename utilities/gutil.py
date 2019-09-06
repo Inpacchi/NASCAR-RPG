0958468e04ca7d18f5781ef693c54dbeb8261c5e
Yovarni Yearwood <thenerfknight@gmail.com>
2019-09-06 00:06:52 -0400

    Implemented code timing function
    Great for determining what code to use and how to code!

--- utilities/gutil.py
+++ utilities/gutil.py
@@ -1,3 +1,5 @@
+from timeit import default_timer as timer
+
 from utilities import futil
 
 
@@ -16,3 +18,38 @@ def importDriversToTeam() -> None:
             teamsDict[driver.teamName].drivers.append(driver.name)
 
     futil.writeDictToJSON('team', teamsDict)
+
+
+def getFunctionTime() -> None:
+    """
+    Runs timeFunction() 25 times and gets the average time it takes to complete a subset of code.
+
+    :return: None
+    :rtype: None
+    """
+
+    timeAverages = []
+
+    i = 0
+    while i < 25:
+        timeAverages.append(timeFunction())
+        i += 1
+
+    average = 0
+    for time in timeAverages:
+        average += time
+
+    print(average / len(timeAverages))
+
+
+def timeFunction() -> float:
+    """
+    Returns the total time taken to run the code
+
+    :return: Total time
+    :rtype: float
+    """
+    start = timer()
+    # INSERT CODE HERE
+    end = timer()
+    return end - start
