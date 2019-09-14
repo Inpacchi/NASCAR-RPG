a0644b74de71486d182065856a824e7039c6bd0e
Yovarni Yearwood <thenerfknight@gmail.com>
2019-09-14 17:09:37 -0400

    Start of website changes

--- webapp/config.py
+++ webapp/config.py
@@ -5,6 +5,7 @@ projectRoot = os.path.abspath(os.path.dirname('..'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'replace-me-in-production'
+    TEMPLATES_AUTO_RELOAD = True
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + f'{projectRoot}\\data\\sqlite\\database.sqlite'
