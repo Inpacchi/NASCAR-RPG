b7efc585b8ff0a5af48856bbb8ad6d88cbab5a19
Yovarni Yearwood <thenerfknight@gmail.com>
2019-09-26 16:14:38 -0400

    Implemented application logging and email functionality

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,6 +3,10 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 from webapp.config import Config
 
 app = Flask(__name__)
@@ -13,3 +17,34 @@ login = LoginManager(app)
 
 from webapp import routes, errors
 from models import *
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        secure = None
+
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='NASRAC-NG Production Alert',
+            credentials=auth, secure=secure)
+
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler('logs/nasrac-ng.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('NASRAC-NG starting up...')
--- webapp/config.py
+++ webapp/config.py
@@ -11,3 +11,14 @@ class Config(object):
         'sqlite:///' + f'{projectRoot}\\data\\sqlite\\database.sqlite'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['internal@endlessgalaxy.dev']
+
+    # FOR TESTING PURPOSES ONLY
+    # MAIL_SERVER = 'localhost'
+    # MAIL_PORT = 8025
