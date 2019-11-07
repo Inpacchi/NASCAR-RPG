ecba1f1ea4d2a1de5de6af9b06472620be111833
Yovarni Yearwood <thenerfknight@gmail.com>
2019-11-07 18:55:15 -0500

    Added separate method for committing changes to database

--- utilities/dbutil.py
+++ utilities/dbutil.py
@@ -1,14 +1,21 @@
+from sqlalchemy.exc import SQLAlchemyError
+
 from models.gameapp import QualifyingResults, RaceResults
 from models.driver import Driver
 
 from webapp import db
 
 
-def commit_standings_to_db(standings):
+def add_standings_to_session(standings):
     for block in standings:
         driver = Driver.query.filter_by(name=block).first()
 
         db.session.add(QualifyingResults(standings[block], driver.id))
         db.session.add(RaceResults(standings[block], driver.id))
 
+
+def commit():
+    try:
         db.session.commit()
+    except SQLAlchemyError:
+        db.session.rollback()
