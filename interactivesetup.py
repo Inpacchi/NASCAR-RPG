3c4f5c623edd06dd71dab41875c4f6c6e3ddfa63
Yovarni Yearwood <thenerfknight@gmail.com>
2019-11-08 17:55:19 -0500

    Interactive Setup imports overhaul

--- interactivesetup.py
+++ interactivesetup.py
@@ -1,10 +1,10 @@
 import importlib
 
-from models.team import Team
+from models.team import Team, TeamCars, TeamRentals, TeamDrivers
 from models.driver import Driver
-from models.gameapp import Schedule, Track
-from game import  *
-from utilities import *
+from models.gameapp import Schedule, Track, QualifyingResults, RaceResults
+from game import potential, raceweekend, test_race_weekend
+from utilities import futil, dbutil
 from webapp import db
 
 print('Interactive Setup Completed!')
